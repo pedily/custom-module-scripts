c9a98a5af6a2f8b9a8b70cf38498a7466e85b352
Robin Schuster <kontakt@robinschuster.net>
2020-03-10 19:28:44 +0100

    add typings for cognigy

--- templates/typescript/src/module.ts
+++ templates/typescript/src/module.ts
@@ -5,22 +5,24 @@
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function add(cognigy: any, args: any) {
+async function add(cognigy: IFlowInput, args: any) {
     const { number1, number2, store, stopOnError } = args;
+    const context = cognigy.context.getFullContext?.();
+
     try {
         const num1 = Number(number1);
         const num2 = Number(number2);
         const sum = num1 + num2;
 
         // write the result into the context at the specified destination
-        cognigy.context.getFullContext()[store] = sum;
+        context[store] = sum;
     } catch (error) {
         // Stop the Flow if 'stopOnError' is set
         if (stopOnError)
             throw error;
 
         // write the error into the context at the specified destination
-        cognigy.context.getFullContext()[store] = {
+        context[store] = {
             error: error
         }
     }
